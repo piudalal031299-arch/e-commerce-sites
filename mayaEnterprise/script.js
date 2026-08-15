2d0cf4409b2a04241c6ab70b30142bfd95ac3d28
Piu Dalal <piudalal031299@gmail.com>
2026-08-15 11:39:05 +0530

    maya site mofified4

--- mayaEnterprise/script.js
+++ mayaEnterprise/script.js
@@ -5,3 +5,20 @@ const ShowNav = document.querySelector("#mobileNav");
 mobileMenuIcon.addEventListener("click",()=>{
     ShowNav.classList.toggle("show");
 })
+
+// active navlink-------------------------------------//
+const currentPage = window.location.pathname.split("/").pop();
+
+document.querySelectorAll(".nav ul li a").forEach(link => {
+    if (link.getAttribute("href") === currentPage) {
+        link.classList.add("active");
+    }
+});
+
+document.querySelectorAll("#mobileNav ul li").forEach(li => {
+    const link = li.querySelector("a");
+
+    if (link.getAttribute("href") === currentPage) {
+        li.classList.add("active");
+    }
+});
